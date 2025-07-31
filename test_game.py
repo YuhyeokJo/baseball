a2dda433fbfa2634fbcd02988fccc3058f06d7b8
yuhyeok <joyu0320@naver.com>
2025-07-31 13:59:07 +0900

    [feature] p.31

--- test_game.py
+++ test_game.py
@@ -2,7 +2,16 @@ from game import Game
 import pytest
 
 
-def test_exception_when_input_is_none():
-    game = Game()
+@pytest.fixture
+def game():
+    return Game()
+
+
+def test_exception_when_input_is_none(game):
     with pytest.raises(TypeError):
         game.guess(None)
+
+
+def test_exception_when_input_length_is_unmatched(game):
+    with pytest.raises(TypeError):
+        game.guess("12")
