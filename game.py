c59cc58b868952c405b811e7b254777f7ce7def0
yuhyeok <joyu0320@naver.com>
2025-07-31 14:20:16 +0900

    [feature] p.52

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
+from game_result import GameResult
+
+
 class Game:
-    def guess(self, guess_number):
+    def guess(self, guess_number) -> GameResult:
         self._assert_illegal_value(guess_number)
+        return GameResult(True, 3, 0)
 
     def _assert_illegal_value(self, guess_number):
         if guess_number is None:
@@ -10,7 +14,7 @@ class Game:
             raise TypeError("입력은 3 자리 문자열이어야 합니다.")
 
         if not guess_number.isdigit():
-                raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
+            raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
 
         if self._id_duplicated_number(guess_number):
             raise TypeError("중복된 숫자가 존재합니다.")
